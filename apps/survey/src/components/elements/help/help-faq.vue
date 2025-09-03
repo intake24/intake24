147a8bda17d542b920a5b8e86a932ffd675a335b
Lukas Hroch <lukashroch@gmail.com>
2025-09-03 13:05:10 +0100

    fix(survey): mobile nav & help (V4-1607)

---
+++ apps/survey/src/components/elements/help/help-faq.vue
@@ -0,0 +1,37 @@
+<template>
+  <slot v-bind="{ to }">
+    <v-btn
+      v-if="mobile"
+      class="order-3 nav-item__help"
+      :to
+      value="help"
+    >
+      <v-icon icon="$info" />
+      <span>{{ $t('common.help._') }}</span>
+    </v-btn>
+    <v-btn
+      v-else
+      color="grey"
+      :title="$t('common.faqs.title')"
+      :to
+      variant="flat"
+    >
+      <v-icon icon="$info" start />
+      {{ $t('common.faqs._') }}
+    </v-btn>
+  </slot>
+</template>
+
+<script lang="ts" setup>
+import { useDisplay } from 'vuetify';
+
+const props = defineProps({
+  surveyId: {
+    type: String,
+    required: true,
+  },
+});
+
+const { mobile } = useDisplay();
+const to = { name: 'faqs', params: { surveyId: props.surveyId } };
+</script>
--- apps/survey/src/components/layouts/navigation.vue
+++ apps/survey/src/components/layouts/navigation.vue
@@ -1,45 +1,104 @@
 <template>
-  <v-bottom-navigation v-if="$vuetify.display.mobile" class="d-print-none" color="primary" grow>
-    <v-btn :to="{ name: 'survey-home', params: { surveyId } }">
-      <v-icon>$home</v-icon>
-      <span>{{ $t('common.home') }}</span>
+  <v-bottom-navigation
+    v-if="mobile"
+    id="app-mobile-navigation"
+    ref="nav"
+    v-model="drawer"
+    class="d-print-none bottom-navigation"
+    color="primary"
+    grow
+  >
+    <v-btn
+      class="order-0"
+      :title="$t('common.nav.home')"
+      :to="{ name: 'survey-home', params: { surveyId } }"
+      value="home"
+    >
+      <v-icon icon="$home" />
+      <span>{{ $t('common.nav.home') }}</span>
     </v-btn>
-    <v-btn v-if="recall" :to="{ name: 'survey-recall', params: { surveyId } }">
-      <v-icon>$survey</v-icon>
-      <span>{{ $t('recall._') }}</span>
+    <v-btn
+      v-if="feedback"
+      class="order-1"
+      :title="$t('common.nav.feedback')"
+      :to="{ name: 'feedback-home', params: { surveyId } }"
+      value="feedback"
+    >
+      <v-icon icon="$feedback" />
+      <span>{{ $t('common.nav.feedback') }}</span>
     </v-btn>
-    <v-btn v-if="feedback" :to="{ name: 'feedback-home', params: { surveyId } }">
-      <v-icon>$feedback</v-icon>
-      <span>{{ $t('feedback._') }}</span>
+    <v-spacer v-else class="order-1" />
+    <v-btn
+      v-if="recall && $route.name !== 'survey-recall'"
+      class="order-2"
+      :title="$t('common.nav.recall')"
+      :to="{ name: 'survey-recall', params: { surveyId } }"
+      value="recall"
+    >
+      <v-icon icon="$survey" />
+      <span>{{ $t('common.nav.recall') }}</span>
     </v-btn>
-    <v-btn :to="{ name: 'survey-profile' }">
-      <v-icon>$profile</v-icon>
-      <span>{{ $t('profile._') }}</span>
+    <help-nav :survey-id @close="removeActiveState('help')" />
+    <v-btn
+      class="order-4 nav-item__options"
+      :title="$t('common.nav.options')"
+      value="options"
+      @click="emit('update:modelValue', true)"
+    >
+      <v-icon icon="fas fa-bars" />
+      <span>{{ $t('common.nav.options') }}</span>
     </v-btn>
   </v-bottom-navigation>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
+import type { PropType } from 'vue';
+import { ref, useTemplateRef, watch } from 'vue';
+import { useDisplay } from 'vuetify';
+import { HelpNav } from '../elements';
 
-export default defineComponent({
-  name: 'AppNavigation',
+defineOptions({ name: 'AppNavigation' });
 
-  props: {
-    surveyId: {
-      type: String,
-      required: true,
-    },
-    recall: {
-      type: Boolean,
-      default: false,
-    },
-    feedback: {
-      type: Boolean,
-      default: false,
-    },
+const props = defineProps({
+  modelValue: {
+    type: Boolean as PropType<boolean>,
   },
+  surveyId: {
+    type: String as PropType<string>,
+    required: true,
+  },
+  recall: {
+    type: Boolean as PropType<boolean>,
+    default: false,
+  },
+  feedback: {
+    type: Boolean as PropType<boolean>,
+    default: false,
+  },
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const { mobile } = useDisplay();
+
+const drawer = ref('home');
+const nav = useTemplateRef('nav');
+
+function removeActiveState(item: string) {
+  const el = (nav.value?.$el as HTMLElement | null)?.querySelector(`.nav-item__${item}`);
+  el?.classList.remove('v-btn--active', 'v-btn--selected', 'text-primary');
+}
+
+watch(() => props.modelValue, (val) => {
+  if (val)
+    return;
+
+  removeActiveState('options');
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.bottom-navigation {
+  overflow: unset;
+}
+</style>
