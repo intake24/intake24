74314843f3102993c606ecafb1eb10b6d02a217a
Lukas Hroch <lukashroch@gmail.com>
2025-10-22 14:19:14 +0100

    fix(fdbs): include securables into food list check (V4-1627)

--- apps/admin/src/views/fdbs/read.vue
+++ apps/admin/src/views/fdbs/read.vue
@@ -21,7 +21,6 @@ import { computed } from 'vue';
 import { Layout } from '@intake24/admin/components/entry';
 import { FoodExplorer } from '@intake24/admin/components/fdbs';
 import { useEntry, useEntryFetch } from '@intake24/admin/composables';
-import { useUser } from '@intake24/admin/stores';
 import type { LocaleEntry } from '@intake24/common/types/http/admin';
 
 defineOptions({ name: 'FoodDBDetail' });
@@ -36,10 +35,9 @@ const props = defineProps({
   },
 });
 
-const user = useUser();
-const readonly = computed(() => !user.can({ action: 'food-list:edit' }));
-
 useEntryFetch(props);
-const { entry, entryLoaded } = useEntry<LocaleEntry>(props);
+const { entry, entryLoaded, canHandleEntry } = useEntry<LocaleEntry>(props);
+
 const code = computed(() => entry.value.code);
+const readonly = computed(() => !canHandleEntry('food-list:edit'));
 </script>
