0e0f85f074f4841e2fa0c00ff2ac186811a19d35
Angus Chiu <anguschiu1@users.noreply.github.com>
2025-12-01 02:13:20 +0000

    refactor(deploy): add DB_TEST_SYSTEM and DB_TEST_FOODS back to docker init script

--- docker/init/001-create-databases.sh
+++ docker/init/001-create-databases.sh
@@ -12,23 +12,6 @@ DB_DEV_FOODS_DATABASE="${DB_DEV_FOODS_DATABASE:=intake24_foods}"
 # System DB
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE ROLE $DB_DEV_SYSTEM_USERNAME WITH PASSWORD '$DB_DEV_SYSTEM_PASSWORD' LOGIN;"
 createdb -U $POSTGRES_USER --owner=$DB_DEV_SYSTEM_USERNAME $DB_DEV_SYSTEM_DATABASE
-# psql -U $POSTGRES_USER -d $DB_DEV_SYSTEM_DATABASE -c "create extension if not exists \"uuid-ossp\";"
-# echo "Restoring system database from snapshot..."
-# pg_restore -n public --no-owner --no-acl --role=$DB_DEV_SYSTEM_USERNAME --dbname $DB_DEV_SYSTEM_DATABASE /docker-entrypoint-initdb.d/system_snapshot.pgcustom
-
-# Insert admin user, password is 'intake24'
-# psql -U $POSTGRES_USER -d $DB_DEV_SYSTEM_DATABASE <<'SQL'
-# insert into users (id, "name", email, phone, simple_name, email_notifications, sms_notifications, multi_factor_authentication, created_at, updated_at, verified_at, disabled_at) 
-# values (default, 'Admin', 'admin@admin.admin', '', 'Admin', true, true, false, now(), now(), now(), null) returning id;
-# insert into user_passwords (user_id, password_hash, password_salt, password_hasher) 
-# values (
-#     11969, 
-#     '$2a$10$metTHvhIsr/srD4RrubR3e48z9gxLseT.a673nlp9Nti4HUE2A3eG', 
-#     '$2a$10$metTHvhIsr/srD4RrubR3e', 
-# 'bcrypt'
-# );
-# insert into role_user (role_id, user_id, created_at, updated_at) values (1, 11969, now(), now());
-# SQL
 
 # Foods DB
 if [ $DB_DEV_FOODS_USERNAME != $DB_DEV_SYSTEM_USERNAME ]; then
@@ -45,34 +28,30 @@ pg_restore -n public --no-owner --no-acl --role=$DB_DEV_FOODS_USERNAME --dbname 
 
 # TEST environment
 
-# DB_TEST_SYSTEM_USERNAME="${DB_TEST_SYSTEM_USERNAME:=intake24}"
-# DB_TEST_SYSTEM_PASSWORD="${DB_TEST_SYSTEM_PASSWORD:=intake24}"
-# DB_TEST_SYSTEM_DATABASE="${DB_TEST_SYSTEM_DATABASE:=intake24_system_test}"
-# DB_TEST_FOODS_USERNAME="${DB_TEST_FOODS_USERNAME:=intake24}"
-# DB_TEST_FOODS_PASSWORD="${DB_TEST_FOODS_PASSWORD:=intake24}"
-# DB_TEST_FOODS_DATABASE="${DB_TEST_FOODS_DATABASE:=intake24_foods_test}"
+DB_TEST_SYSTEM_USERNAME="${DB_TEST_SYSTEM_USERNAME:=intake24}"
+DB_TEST_SYSTEM_PASSWORD="${DB_TEST_SYSTEM_PASSWORD:=intake24}"
+DB_TEST_SYSTEM_DATABASE="${DB_TEST_SYSTEM_DATABASE:=intake24_system_test}"
+DB_TEST_FOODS_USERNAME="${DB_TEST_FOODS_USERNAME:=intake24}"
+DB_TEST_FOODS_PASSWORD="${DB_TEST_FOODS_PASSWORD:=intake24}"
+DB_TEST_FOODS_DATABASE="${DB_TEST_FOODS_DATABASE:=intake24_foods_test}"
 
-# # System DB
-# if [ "$DB_TEST_SYSTEM_USERNAME" ] && [ "$DB_TEST_SYSTEM_DATABASE" ]; then
-#     if [ $DB_TEST_SYSTEM_USERNAME != $DB_DEV_SYSTEM_USERNAME ]; then
-#         psql -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE ROLE $DB_TEST_SYSTEM_USERNAME WITH PASSWORD '$DB_TEST_SYSTEM_PASSWORD' LOGIN;"
-#     fi
+# System DB
+if [ "$DB_TEST_SYSTEM_USERNAME" ] && [ "$DB_TEST_SYSTEM_DATABASE" ]; then
+    if [ $DB_TEST_SYSTEM_USERNAME != $DB_DEV_SYSTEM_USERNAME ]; then
+        psql -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE ROLE $DB_TEST_SYSTEM_USERNAME WITH PASSWORD '$DB_TEST_SYSTEM_PASSWORD' LOGIN;"
+    fi
 
-#     createdb -U $POSTGRES_USER --owner=$DB_TEST_SYSTEM_USERNAME $DB_TEST_SYSTEM_DATABASE;
-#     psql -U $POSTGRES_USER -d $DB_TEST_SYSTEM_DATABASE -c "create extension if not exists \"uuid-ossp\";"
-#     echo "Restoring test system database from snapshot..."
-#     pg_restore -n public --no-owner --no-acl --role=$DB_TEST_SYSTEM_USERNAME --dbname $DB_TEST_SYSTEM_DATABASE /docker-entrypoint-initdb.d/system_test_snapshot.pgcustom
-# fi
+    createdb -U $POSTGRES_USER --owner=$DB_TEST_SYSTEM_USERNAME $DB_TEST_SYSTEM_DATABASE;
+    psql -U $POSTGRES_USER -d $DB_TEST_SYSTEM_DATABASE -c "create extension if not exists \"uuid-ossp\";"
+fi
 
-# # Foods DB
-# if [ "$DB_TEST_FOODS_USERNAME" ] && [ "$DB_TEST_FOODS_DATABASE" ]; then
-#     if [ $DB_TEST_FOODS_USERNAME != $DB_DEV_SYSTEM_USERNAME ]; then
-#         psql -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE ROLE $DB_TEST_FOODS_USERNAME WITH PASSWORD '$DB_TEST_FOODS_PASSWORD' LOGIN;"
-#     fi
+# Foods DB
+if [ "$DB_TEST_FOODS_USERNAME" ] && [ "$DB_TEST_FOODS_DATABASE" ]; then
+    if [ $DB_TEST_FOODS_USERNAME != $DB_DEV_SYSTEM_USERNAME ]; then
+        psql -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE ROLE $DB_TEST_FOODS_USERNAME WITH PASSWORD '$DB_TEST_FOODS_PASSWORD' LOGIN;"
+    fi
 
-#     createdb -U $POSTGRES_USER --owner=$DB_TEST_FOODS_USERNAME $DB_TEST_FOODS_DATABASE;
-#     psql -U $POSTGRES_USER -d $DB_TEST_FOODS_DATABASE -c "create extension if not exists \"uuid-ossp\";"
-#     psql -U $POSTGRES_USER -d $DB_TEST_FOODS_DATABASE -c "create extension if not exists \"btree_gist\";"
-#     echo "Restoring test foods database from snapshot..."
-#     pg_restore -n public --no-owner --no-acl --role=$DB_TEST_FOODS_USERNAME --dbname $DB_TEST_FOODS_DATABASE /docker-entrypoint-initdb.d/foods_test_snapshot.pgcustom
-# fi
+    createdb -U $POSTGRES_USER --owner=$DB_TEST_FOODS_USERNAME $DB_TEST_FOODS_DATABASE;
+    psql -U $POSTGRES_USER -d $DB_TEST_FOODS_DATABASE -c "create extension if not exists \"uuid-ossp\";"
+    psql -U $POSTGRES_USER -d $DB_TEST_FOODS_DATABASE -c "create extension if not exists \"btree_gist\";"
+fi
