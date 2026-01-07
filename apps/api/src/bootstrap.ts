d4cd2373ef14054b5e21adda285277b7f679a86c
Ivan Poliakov <ivpolyakov@gmail.com>
2026-01-07 17:58:29 +0000

    feat(api): check for existence of .env file and show nicer error message

--- apps/api/src/bootstrap.ts
+++ apps/api/src/bootstrap.ts
@@ -1,10 +1,25 @@
-import { resolve } from 'node:path';
+import fs from 'node:fs';
 
+import { resolve } from 'node:path';
 import dotenv from 'dotenv';
 import dotenvExpand from 'dotenv-expand';
 
+function checkEnvFileExists(path: string): void {
+  try {
+    fs.accessSync(path);
+  }
+  catch (err) {
+    throw new Error(`.env file does not exist -- expected at ${path}\n\nSee https://docs.intake24.org/config/\n`, { cause: err });
+  }
+}
+
+const envFilePath
+  = process.env.NODE_ENV === 'test' ? resolve(__dirname, '../__tests__/.env-test') : resolve(__dirname, '../.env');
+
+checkEnvFileExists(envFilePath);
+
 const options
-  = process.env.NODE_ENV === 'test' ? { path: resolve(__dirname, '../__tests__/.env-test') } : undefined;
+  = process.env.NODE_ENV === 'test' ? { path: envFilePath } : undefined;
 
 const dotEnv = dotenv.config(options);
 dotenvExpand.expand(dotEnv);
