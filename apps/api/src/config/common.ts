5ec5044900a2f4b64c92a83ec89e8eaac8828711
Ivan Poliakov <ivpolyakov@gmail.com>
2026-01-07 17:55:55 +0000

    fix(api): cookie lifetime validation and defaults

--- apps/api/src/config/common.ts
+++ apps/api/src/config/common.ts
@@ -2,7 +2,12 @@ import type { StringValue } from 'ms';
 import ms from 'ms';
 import z from 'zod';
 
-export const msStringValue = z.custom<StringValue>(() => z.string().regex(/^\d+([smhdwy]|sec|secs|second|seconds|min|mins|minute|minutes|hr|hrs|hour|hours|day|days|week|weeks|yr|yrs|year|years)$/));
+// Eslint offers incorrect replacement: this regex matches zero or one spaces but eslint wants to change it to " *" which matches any number of spaces.
+// eslint-disable-next-line regexp/no-trivially-nested-quantifier
+const msRegex = /^\d+(?: +)?(?:milliseconds?|msecs?|ms|seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|weeks?|w|years?|yrs?|y)?$/i;
+
+export const msStringValue = z.custom<StringValue>(val => typeof val === 'string' && msRegex.test(val), 'Expected a value in vercel-ms format (e.g., \'30s\', \'1d\'). See https://github.com/vercel/ms');
+
 export const parsedMsStringValue = msStringValue.transform(stringValue => ms(stringValue));
 
 export const sameSiteCookieOptions = ['strict', 'lax', 'none'] as const;
