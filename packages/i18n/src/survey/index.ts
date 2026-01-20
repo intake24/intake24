de905a13a0c15ece93b9179c0ca18bf08e8e8af1
Lukas Hroch <lukashroch@gmail.com>
2026-01-20 10:18:19 +0000

    feat(i18n): chichewa language (V4-1701)

--- packages/i18n/src/survey/index.ts
+++ packages/i18n/src/survey/index.ts
@@ -2,9 +2,18 @@ import ar_AE from './ar-AE';
 import en from './en';
 import fr from './fr';
 import ms from './ms';
+import ny from './ny';
 import ta from './ta';
 import zh from './zh';
 
-const messages = { 'ar-AE': ar_AE, en, zh, ta, ms, fr };
+const messages = {
+  'ar-AE': ar_AE,
+  en,
+  zh,
+  ta,
+  ms,
+  ny,
+  fr,
+};
 
 export default messages;
