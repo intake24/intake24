c34600598428413abfc5d9c3b5829f84fe42a664
Lukas Hroch <lukashroch@gmail.com>
2026-03-24 13:08:14 +0000

    fix(jobs): small UI cleanup to align with other job params

--- apps/admin/src/components/jobs/params/survey-nutrients-recalculation.vue
+++ apps/admin/src/components/jobs/params/survey-nutrients-recalculation.vue
@@ -1,76 +1,62 @@
 <template>
   <div class="d-flex flex-column ga-4">
-    <v-card-title>{{ $t('jobs.params') }}</v-card-title>
-    <v-card-text>
-      <v-row>
-        <v-col cols="12">
-          <select-resource
-            v-if="!disabled.surveyId"
-            v-model="params.surveyId"
-            :error-messages="errors.get('params.surveyId')"
-            :label="$t('jobs.types.SurveyNutrientsRecalculation.surveyId')"
-            name="surveyId"
-            resource="surveys"
-          />
-        </v-col>
-        <v-col cols="12">
-          <v-select
-            v-model="params.mode"
-            :error-messages="errors.get('params.mode')"
-            hide-details="auto"
-            item-title="text"
-            item-value="value"
-            :items="modeOptions"
-            :label="$t('jobs.types.SurveyNutrientsRecalculation.mode.label')"
-            name="mode"
-            variant="outlined"
-          >
-            <template #item="{ item, props }">
-              <v-list-item v-bind="props">
-                <template #subtitle>
-                  <div class="text-caption">
-                    {{ item.raw.description }}
-                  </div>
-                </template>
-              </v-list-item>
-            </template>
-          </v-select>
-        </v-col>
-        <v-col v-if="params.mode !== 'none'" cols="12">
-          <v-checkbox
-            v-model="params.syncFields"
-            :error-messages="errors.get('params.syncFields')"
-            hide-details="auto"
-          >
-            <template #label>
-              <div>
-                <div>{{ $t('jobs.types.SurveyNutrientsRecalculation.syncFields.label') }}</div>
-                <div class="text-caption text-medium-emphasis">
-                  {{ $t('jobs.types.SurveyNutrientsRecalculation.syncFields.description') }}
-                </div>
-              </div>
-            </template>
-          </v-checkbox>
-        </v-col>
-      </v-row>
-
-      <v-alert
-        v-if="params.mode === 'values-and-codes'"
-        class="mt-4"
-        color="warning"
-        variant="tonal"
-      >
-        <v-alert-title>{{ $t('jobs.types.SurveyNutrientsRecalculation.mappingChanges.title') }}</v-alert-title>
-        <div class="text-body-2">
-          {{ $t('jobs.types.SurveyNutrientsRecalculation.mappingChanges.description') }}
-          <ul class="mt-2 list-disc pl-4">
-            <li>{{ $t('jobs.types.SurveyNutrientsRecalculation.mappingChanges.impact.historicalData') }}</li>
-            <li>{{ $t('jobs.types.SurveyNutrientsRecalculation.mappingChanges.impact.exportedDatasets') }}</li>
-          </ul>
-          {{ $t('jobs.types.SurveyNutrientsRecalculation.mappingChanges.warning') }}
+    <select-resource
+      v-if="!disabled.surveyId"
+      v-model="params.surveyId"
+      :error-messages="errors.get('params.surveyId')"
+      :label="$t('jobs.types.SurveyNutrientsRecalculation.surveyId')"
+      name="surveyId"
+      resource="surveys"
+    />
+    <v-select
+      v-model="params.mode"
+      :error-messages="errors.get('params.mode')"
+      item-title="text"
+      item-value="value"
+      :items="modeOptions"
+      :label="$t('jobs.types.SurveyNutrientsRecalculation.mode.label')"
+      name="mode"
+    >
+      <template #item="{ item, props }">
+        <v-list-item v-bind="props">
+          <template #subtitle>
+            <div class="text-caption">
+              {{ item.raw.description }}
+            </div>
+          </template>
+        </v-list-item>
+      </template>
+    </v-select>
+    <v-checkbox
+      v-if="params.mode !== 'none'"
+      v-model="params.syncFields"
+      :error-messages="errors.get('params.syncFields')"
+    >
+      <template #label>
+        <div>
+          <div>{{ $t('jobs.types.SurveyNutrientsRecalculation.syncFields.label') }}</div>
+          <div class="text-caption text-medium-emphasis">
+            {{ $t('jobs.types.SurveyNutrientsRecalculation.syncFields.description') }}
+          </div>
         </div>
-      </v-alert>
-    </v-card-text>
+      </template>
+    </v-checkbox>
+    <v-alert
+      v-if="params.mode === 'values-and-codes'"
+      class="mt-4"
+      color="warning"
+      variant="tonal"
+    >
+      <v-alert-title>{{ $t('jobs.types.SurveyNutrientsRecalculation.mappingChanges.title') }}</v-alert-title>
+      <div class="text-body-2">
+        {{ $t('jobs.types.SurveyNutrientsRecalculation.mappingChanges.description') }}
+        <ul class="mt-2 list-disc pl-4">
+          <li>{{ $t('jobs.types.SurveyNutrientsRecalculation.mappingChanges.impact.historicalData') }}</li>
+          <li>{{ $t('jobs.types.SurveyNutrientsRecalculation.mappingChanges.impact.exportedDatasets') }}</li>
+        </ul>
+        {{ $t('jobs.types.SurveyNutrientsRecalculation.mappingChanges.warning') }}
+      </div>
+    </v-alert>
   </div>
 </template>
 
