29b07615084a78fee592db408d89f82a31efab39
Lukas Hroch <lukashroch@gmail.com>
2026-05-12 17:41:29 +0100

    fix(data-export-mapper): support deep prop extraction for psm

--- apps/api/src/services/admin/data-export/data-export-mapper.ts
+++ apps/api/src/services/admin/data-export/data-export-mapper.ts
@@ -5,7 +5,7 @@ import type {
   ExportRow,
 } from './data-export-fields';
 import type { IoC } from '@intake24/api/ioc';
-import type { CustomPromptAnswer, ExportSectionId, PortionSizeState } from '@intake24/common/surveys';
+import type { CustomPromptAnswer, ExportSectionId } from '@intake24/common/surveys';
 
 import { get } from 'lodash-es';
 
@@ -51,7 +51,7 @@ export function foodNutrientValue(field: ExportField): ExportFieldTransform {
 
 export function portionSizeValue(field: ExportField): ExportFieldTransform {
   return ({ food }) =>
-    !food.portionSize || typeof food.portionSize === 'string' ? undefined : food.portionSize[field.id as keyof PortionSizeState];
+    !food.portionSize || typeof food.portionSize === 'string' ? undefined : get(food.portionSize, field.id);
 }
 
 export function externalSourceField(field: ExportField): ExportFieldTransform {
